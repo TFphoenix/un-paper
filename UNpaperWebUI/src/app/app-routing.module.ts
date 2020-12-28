466d71de696bbac5ed0780baa63dec9f22ded65c
Teodor <teodor@mihaescu.info>
2020-12-28 11:09:31 +0200

    UWU: Changing structure...

--- UNpaperWebUI/src/app/app-routing.module.ts
+++ UNpaperWebUI/src/app/app-routing.module.ts
@@ -4,52 +4,49 @@ import { HomeComponent } from './core/components/home/home.component';
 import { EmptyLayoutComponent } from './core/layouts/empty-layout/empty-layout.component';
 import { MainLayoutComponent } from './core/layouts/main-layout/main-layout.component';
 
-// const ROUTES: Routes = [
-//   // main layout routes
-//   {
-//     path: '',
-//     component: MainLayoutComponent,
-//     canActivateChild: [], //TODO: Here will general guards be declared (like EULA, SIGN-UP, etc.)
-//     children: [
-//       {
-//         path: '',
-//         component: HomeComponent,
-//         pathMatch: 'full'
-//       }
-//     ]
-//   },
-//   // empty layout routes
-//   {
-//     path: '',
-//     component: EmptyLayoutComponent,
-//     children: [
-//       // TODO: auth-callback
-//     ]
-//   },
-//   // no layout routes
-//   { path: '**', redirectTo: '/' } // all unhandled routes redirect to this
-// ];
-
 const ROUTES: Routes = [
-  {
-    // Needed for hash routing
-    path: 'error',
-    component: HomeComponent
-  },
-  {
-    // Needed for hash routing
-    path: 'state',
-    component: HomeComponent
-  },
-  {
-    // Needed for hash routing
-    path: 'code',
-    component: HomeComponent
-  },
+  // main layout routes
   {
     path: '',
-    component: HomeComponent
-  }
+    component: MainLayoutComponent,
+    canActivateChild: [], //TODO: Here will general guards be declared (like EULA, SIGN-UP, etc.)
+    children: [
+      {
+        // Needed for hash routing
+        path: 'error',
+        component: HomeComponent
+      },
+      {
+        // Needed for hash routing
+        path: 'state',
+        component: HomeComponent
+      },
+      {
+        // Needed for hash routing
+        path: 'code',
+        component: HomeComponent
+      },
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: '',
+        component: HomeComponent,
+        pathMatch: 'full'
+      }
+    ]
+  },
+  // empty layout routes
+  {
+    path: '',
+    component: EmptyLayoutComponent,
+    children: [
+      // TODO: auth-callback
+    ]
+  },
+  // no layout routes
+  { path: '**', redirectTo: '/' } // all unhandled routes redirect to this
 ];
 
 const isIframe = window !== window.parent && !window.opener;
